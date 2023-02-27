0765b1604663d46e3caaf8afa89443f32b571c03
gengstrand <gengstrand@gmail.com>
2023-02-26 19:03:10 -0800

    still trying to get GA working

--- assets/ga.js
+++ assets/ga.js
@@ -1,9 +1,5 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JMTSMFHZKD"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JMTSMFHZKD');
 
-  gtag('config', 'G-JMTSMFHZKD');
-</script>
